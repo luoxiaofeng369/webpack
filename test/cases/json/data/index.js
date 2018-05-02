2e1b30567def555b62cd9de46187c7968978a492
Tobias Koppers <tobias.koppers@googlemail.com>
2018-05-02 14:27:19 +0200

    introduce error

--- test/cases/json/data/index.js
+++ test/cases/json/data/index.js
@@ -3,7 +3,7 @@ it("should require json via require", function() {
 	expect({ data: require("./b.json") }).toEqual({ data: 123 });
 	expect({ data: require("./c.json") }).toEqual({ data: [1, 2, 3, 4] });
 	expect({ data: require("./e.json") }).toEqual({ data: {
-		"aa": 1,
+		"aa": 123,
 		"bb": 2,
 		"1": "x"
 	} });
